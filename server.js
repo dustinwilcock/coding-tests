8fd64a6863acca7aaeea0d3ea2c6237568c5736a
Dustin Wilcock <dustin.wilcock@imaginelearning.com>
2022-05-27 16:30:32 -0600

    PATCH Company name

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const db = require('./db');
+app.use(express.json());
 
 /**
  * GET /companies
@@ -52,11 +53,40 @@ app.get('/companies/:companyId', (req, res) => {
 })
 
 /**
+ * PATCH /companies/:companyId
+ * Allows edit of company name to be saved to the database
+ */
+ app.patch('/companies/:companyId', (req, res) => {
+	const updateCompany = req.body;
+	if (updateCompany.name == undefined)
+		res.status(400).json({ 
+			error: "name is undefined"
+		});
+
+	const sql = `
+		UPDATE companies SET name = @name WHERE id = @companyId;
+	`;
+	const params = [ updateCompany.name, req.params.companyId ];
+
+	db.run(sql, params, (err,row) => {
+		if (err) {
+			res.status(400).json({
+				error: err.message
+			});
+			return;
+		}
+		res.status(204).json({
+			message: 'success'
+		})
+	});
+})
+
+/**
  * GET /companies/:companyId/departments
  */
 app.get('/companies/:companyId/departments', (req, res) => {
 	const sql = `
-		SELECT * FROM departments WHERE company_id = @companyId
+		SELECT d.id, d.Name FROM departments d WHERE company_id = @companyId;
 	`;
 	const params = req.params.companyId;
 
@@ -75,11 +105,11 @@ app.get('/companies/:companyId/departments', (req, res) => {
 })
 
 /**
- * GET /companies/:companyId/departments/:departmentId/employees
+ * GET /departments/:departmentId/employees
  */
  app.get('/departments/:departmentId/employees', (req, res) => {
 	const sql = `
-		SELECT * FROM employees WHERE department_id = @departmentId
+		SELECT e.id, e.name, e.avatar, e.title, e.country FROM employees e WHERE department_id = @departmentId;
 	`;
 	const params = req.params.departmentId;
 
